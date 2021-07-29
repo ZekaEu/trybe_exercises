f7d2d5d7f8682576e6a2adc49b544a3355238193
ZekaEu <zeka.surf@hotmail.com>
2021-07-29 20:47:01 -0300

    exercicios do dia em arquivo scripts.js

---
+++ trybe-exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,101 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//exercicio 1
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//    console.log(numbers[i]);
+// }
+
+//exercicio 2
+
+// let resultado = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     resultado += numbers[i];
+// }
+
+// console.log(resultado);
+
+//exercicio 3
+
+// let resultado = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     resultado += numbers[i];
+// }
+
+// console.log(resultado / numbers.length)
+
+//exercicio 4
+
+// let resultado = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     resultado += numbers[i];
+// }
+
+// if (resultado / numbers.length > 20) {
+//     console.log("Valor maior que 20!");
+// }
+// else {
+//     console.log("Valor menor ou igual a 20...");
+// }
+
+//exercicio 5
+
+// let maiorNum = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (maiorNum < numbers[i]) {
+//         maiorNum = numbers[i];
+//     }
+// }
+// console.log(maiorNum);
+
+//exercicio 6
+
+// let impar = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 == 1) {
+//         impar += 1;
+//     }
+// }
+// if (impar > 0) {
+//     console.log(impar);
+// }
+// else {
+//     console.log("Nenhum calor impar encontrado");
+// }
+
+//exercicio 7
+
+// let menorNum = numbers[0];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (menorNum > numbers[i]) {
+//         menorNum = numbers[i];
+//     }
+// }
+// console.log(menorNum);
+
+//exercicio 8
+
+// let ToLife = []
+
+// for (let i = 0; i < 25; i += 1) {
+//     ToLife.push(i + 1);
+// }
+
+// console.log(ToLife);
+
+//exercicio 9
+
+// let ToLife = []
+
+// for (let i = 0; i < 25; i += 1) {
+//     ToLife.push(i + 1);
+//     ToLife[i] /= 2;
+// }
+
+// console.log(ToLife);
