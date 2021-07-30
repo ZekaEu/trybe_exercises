04f385745612ab71bb2ba5012ede6b9a5d38fc62
ZekaEu <zeka.surf@hotmail.com>
2021-07-30 19:06:53 -0300

    exercicio 2

--- trybe-exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ trybe-exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,10 +1,24 @@
 //exercicio 1
 
+// let n = 5;
+// let aster = "";
+
+// for (let i = 0; i <= n; i += 1) {
+//     for (let j = 0; j < n; j += 1) {
+//         aster += "*";
+//     }
+//     aster += "\n";
+// }
+
+// console.log(aster);
+
+//exercicio 2
+
 let n = 5;
 let aster = "";
 
 for (let i = 0; i <= n; i += 1) {
-    for (let j = 0; j < n; j += 1) {
+    for (let j = 0; j < i; j += 1) {
         aster += "*";
     }
     aster += "\n";
