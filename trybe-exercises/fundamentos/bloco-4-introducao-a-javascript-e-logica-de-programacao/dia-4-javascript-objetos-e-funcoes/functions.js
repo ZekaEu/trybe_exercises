1ec18d696e2d2eb71e25531a0ab22877df552554
ZekaEu <zeka.surf@hotmail.com>
2021-08-02 19:11:37 -0300

    adiciona exercicio 2

--- trybe-exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/functions.js
+++ trybe-exercises/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/functions.js
@@ -1,12 +1,27 @@
 //exercicio 1
 
-function verPalindrome (string) {
-  for (i in string) {
-    if (string[i] != string[(string.length -1) - i]) {
-      return false;
+// function verPalindrome (string) {
+//   for (i in string) {
+//     if (string[i] != string[(string.length -1) - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//
+// console.log(verPalindrome("arara"));
+// console.log(verPalindrome("desenvolvimento"));
+
+//exercicio 2
+
+function iMaiorNum (numbers) {
+  let iDoMaior = 0;
+  for (let i in numbers) {
+    if (numbers[iDoMaior] < numbers[i]) {
+      iDoMaior = i;
     }
   }
-  return true;
+  return iDoMaior;
 }
-console.log(verPalindrome("arara"));
-console.log(verPalindrome("desenvolvimento"));
+
+console.log(iMaiorNum([2, 4, 6, 7, 10, 0, -3]));
