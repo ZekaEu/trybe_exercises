e753012a550de050f6fbf1013fed8751537095b6
ZekaEu <zeka.surf@hotmail.com>
2021-08-09 18:46:02 -0300

    cria html,js e adiciona exercicios do 1 ao 4

---
+++ trybe-exercises/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
@@ -0,0 +1,17 @@
+//exercicio 1.  
+//   document.getElementsByTagName('p')[0].innerHTML = 'Daqui dois anos estarei trabalhando com COBOL.';
+
+//exercicio 2.
+// document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+
+//exercicio 3.
+//  document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+
+//exercicio 4.
+// document.getElementsByTagName('h1')[0].innerHTML = 'Exercício 5.1 - JavaScript';
+
+//exercicio 5.
+// document.getElementsByTagName('p').style.textTransform = 'uppercase';
+
+//exercicio 6.
+ console.log(document.getElementsByTagName('p').innerText);
